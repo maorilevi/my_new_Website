f62a92775ff1f6392c174ced9d55dad8ff3bb57e
maorl <maorlevi1990@gmail.com>
2017-10-24 15:43:55 +0300

    new update 24-10-2017 v1.00 15:43

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'nice';
+  images;
+  constructor(public renderer: Renderer2, public element_ref: ElementRef) {}
+  // get object by id from html page
+  @ViewChild('myTopnav') navTrigger: ElementRef;
+ngOnInit() {
+this.images = [
+    {'url': 'http://your_image1_url'},
+    {'url': 'http://your_image2_url'},
+    {'url': 'http://your_image3_url'},
+    {'url': 'http://your_image4_url'},
+    {'url': 'http://your_image5_url'},
+    {'url': 'http://your_image6_url'},
+    {'url': 'http://your_image7_url'},
+    {'url': 'http://your_image8_url'},
+    {'url': 'http://your_image9_url'},
+    {'url': 'http://your_image10_url'},
+    {'url': 'http://your_image11_url'},
+    {'url': 'http://your_image12_url'}];
+}
+
+  myFunction() {
+    const x = this.navTrigger.nativeElement;
+    if (x.className === 'topnav') {
+      this.renderer.addClass(x, 'responsive');
+    } else {
+      this.renderer.removeClass(x, 'responsive');
+    }
+  }
 }
